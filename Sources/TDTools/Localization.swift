25ec29425b422e1266874225c8d05f098b0c21c3
Tieda Wei <weitieda@gmail.com>
2019-09-29 00:04:57 -0400

    add localization

---
+++ Sources/TDTools/Localization.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tieda Wei on 2019-09-29.
+//
+
+import Foundation
+
+public func l(_ stringKey: String) -> String{
+    return NSLocalizedString(stringKey, comment: "")
+}
